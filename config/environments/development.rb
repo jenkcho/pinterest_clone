4486ec46a50cb543dbf650de9cddb8f292e41d62
jenkcho <jenkcho@gmail.com>
2017-10-30 01:45:50 +0000

    Associated pins with userds and added Authentication for users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #config.web_console.whitelisted_ips << "24.6.254.120" # cloud9's preview network
+  config.action_mailer.default_url_options = {host:'https://jenkcho-pinterested.herokuapp.com/'}
+
+  config.serve_static_files = true
 end
